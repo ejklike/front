bf6208b64ece175320f2c26a24fa4529ab35733d
TimidLion <sgm02186@naver.com>
2017-02-23 03:02:22 +0900

    Review Added and make detail variable

--- src/components/DetailInfo/DetailInfo.js
+++ src/components/DetailInfo/DetailInfo.js
@@ -3,6 +3,7 @@ import styles from './DetailInfo.css';
 import noImg from '../../../img/no-image.jpg';
 import tabelogLogo from '../../../img/logos/tabelogLogo.png';
 import tripadvisorLogo from '../../../img/logos/tripadvisorLogo.png';
+import Review from '../Review/Review';
 
 class OpeningHours extends React.Component {
   constructor(props){
@@ -17,48 +18,6 @@ class OpeningHours extends React.Component {
             <div key={i}>{day}</div>
           );
         })}
-      </div>
-    );
-  }
-}
-
-class DetailImg extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      havePhoto: false,
-      imgSrc: ''
-    }
-
-    this.getImgSrc = this.getImgSrc.bind(this);
-    this.showImg = this.showImg.bind(this);
-  }
-
-  componentDidMount(){
-    this.getImgSrc(this.props.photos);
-  }
-
-  getImgSrc(photos){
-    console.log(photos);
-    if (typeof photos == 'undefined'){
-      console.log('undefined');
-    }
-    else {
-      this.setState({
-        imgSrc: this.props.photos[0].getUrl({'maxWidth': 280, 'maxHeight': 250})
-      });
-      console.log(this.state.imgSrc);
-    }
-  }
-
-  showImg(){
-    console.log('showimg : ', this.state.imgSrc);
-  }
-
-  render(){
-    return(
-      <div>
-        img in here
       </div>
     );
   }
@@ -82,46 +41,55 @@ class DetailInfo extends React.Component {
   }
 
 	render() {
-		return(
-      <div>
-        <div className={styles.placeName}>
-          <a target="_blank" href={this.props.detail.website ? this.props.detail.website : '/'}>
-            <b>{this.props.detail.name}</b>
-          </a>
-        </div>
+    var detail = this.props.detail;
+  		return(
         <div>
-          <DetailImg photos={this.props.detail.photos} />
-          <img width="270" height="200" className={styles.placeImg} 
-                     src={typeof this.props.detail.photos !== 'undefined'? this.props.detail.photos[0].getUrl({'maxWidth': 280, 'maxHeight': 200}) : noImg} />
-        </div>
-        <div id="storeInfo">
-            <div className="menu-item"> 전화번호 : {this.props.detail.international_phone_number ? this.props.detail.international_phone_number : 'None'}</div>
-          <div id="opening_hours">
-            {this.props.detail.opening_hours &&
-              <a className="menu-item" onClick={this.onClick} href='#' >
-                운영 시간 보기</a>}
-          <br />
-            {this.state.showOpeningHours && 
-
-              this.props.detail.opening_hours.weekday_text && 
-              <OpeningHours 
-                weekday_text={this.props.detail.opening_hours.weekday_text} 
-              />
-            }
+          <div className={styles.placeName}>
+            <a target="_blank" href={detail.website ? detail.website : '/'}>
+              <b>{detail.name}</b>
+            </a>
           </div>
-         </div>
-         <div>
-           {
-             this.props.detail.tabelog_rating != -1 &&
-              <div>
-                <img src={tabelogLogo}/> {this.props.detail.tabelog_rating}
-                <br/>
-              </div>
-           }
-            <img src={tripadvisorLogo}/> {this.props.detail.tripadvisor_rating}
-         </div>
-      </div>
-    );
+          <div>
+            <img width="270" height="200" className={styles.placeImg} 
+                       src={typeof detail.photos !== 'undefined'? detail.photos[0].getUrl({'maxWidth': 280, 'maxHeight': 200}) : noImg} />
+          </div>
+          <div id="storeInfo">
+              <div className="menu-item"> 전화번호 : {detail.international_phone_number ? detail.international_phone_number : 'None'}</div>
+            <div id="opening_hours">
+              {this.props.detail.opening_hours &&
+                <a className="menu-item" onClick={this.onClick} href='#' >
+                  운영 시간 보기</a>}
+            <br />
+              {this.state.showOpeningHours && 
+
+                detail.opening_hours.weekday_text && 
+                <OpeningHours 
+                  weekday_text={detail.opening_hours.weekday_text} 
+                />
+              }
+            </div>
+           </div>
+           <div>
+             {
+               detail.tabelog_rating != -1 &&
+                <div>
+                  <img src={tabelogLogo}/> {detail.tabelog_rating}
+                  <br/>
+                </div>
+             }
+              <img src={tripadvisorLogo}/> {detail.tripadvisor_rating}
+           </div>
+           <div id="Reviews">
+              {detail.reviews &&
+                detail.reviews.map((review,i) => {
+                return(
+                    <Review key={i} review={review}/>
+                );
+                })
+              }
+           </div>
+        </div>
+      );
   }
 }
 
