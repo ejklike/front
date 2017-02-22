f4d79e8038da8d311eb50d3ec7bf25aa8321a8b1
TimidLion <sgm02186@naver.com>
2017-02-23 04:02:36 +0900

    자간 간격 merged with Review 코드

--- src/components/DetailInfo/DetailInfo.js
+++ src/components/DetailInfo/DetailInfo.js
@@ -41,64 +41,65 @@ class DetailInfo extends React.Component {
 	render() {
     var detail = this.props.detail;
   		return(
-        <div>
-          <div className={styles.placeName}>
-            <a target="_blank" href={detail.website ? detail.website : '/'}>
-              <b>{detail.name}</b>
-            </a>
-          </div>
+        <div> 
           <div>
-            <img width="270" height="200" className={styles.placeImg} 
-                       src={typeof detail.photos !== 'undefined'? detail.photos[0].getUrl({'maxWidth': 280, 'maxHeight': 200}) : noImg} />
-          </div>
-          <div id="storeInfo">
-              <div className="menu-item"> 전화번호 : {detail.international_phone_number ? detail.international_phone_number : 'None'}</div>
-            <div id="opening_hours">
+            <div className={styles.placeName}>
+              <a target="_blank" href={detail.website ? detail.website : '/'}>
+                <b>{detail.name}</b>
+              </a>
+            </div>
+    
+            <div>
+             <img width="270" height="200" className={styles.placeImg} 
+                         src={typeof detail.photos !== 'undefined'? detail.photos[0].getUrl({'maxWidth': 280, 'maxHeight': 200}) : noImg} />
+            </div>
+    
+            <div>
+              {
+                this.props.detail.international_phone_number&&
+                <div className={styles.menuItem}>
+                  <div className={styles.sectionName}>전화번호</div>
+                  <div className={styles.sectionContent}>
+                    {this.props.detail.international_phone_number ? this.props.detail.international_phone_number : 'None'}<br/>
+                  </div>
+                </div>
+              }
+              <div>
               {this.props.detail.opening_hours &&
-                <a className="menu-item" onClick={this.onClick} href='#' >
-                  운영 시간 보기</a>}
-            <br />
-              {this.state.showOpeningHours && 
-
-                detail.opening_hours.weekday_text && 
-                <OpeningHours 
-                  weekday_text={detail.opening_hours.weekday_text} 
+                  <a className={styles.sectionName} onClick={this.onClick} href='#' >운영 시간 보기<br/></a>
+              }
+              {
+                this.state.showOpeningHours && 
+                this.props.detail.opening_hours.weekday_text && 
+                <OpeningHours className={styles.sectionContent}
+                  weekday_text={this.props.detail.opening_hours.weekday_text} 
                 />
               }
+              </div>
             </div>
-           </div>
-             <div>
-                {
-                  this.props.detail.international_phone_number&&
-                  <div className={styles.menuItem}>
-                    <div className={styles.sectionName}>전화번호</div>
-                    <div className={styles.sectionContent}>
-                      {this.props.detail.international_phone_number}<br/>
-                    </div>
-                  </div>
-                }
-                <div>
-                  {this.props.detail.opening_hours &&
-                      <a className={styles.sectionName} onClick={this.onClick} href='#' >운영 시간 보기 <img className={styles.logoImg} src='./assets/img/icons/arrow.png'/><br/></a>
-                  }
-                  {
-                    this.state.showOpeningHours && 
-                    this.props.detail.opening_hours.weekday_text && 
-                    <OpeningHours className={styles.sectionContent}
-                      weekday_text={this.props.detail.opening_hours.weekday_text} 
-                    />
-                  }
+
+            <div>
+              {
+                typeof this.props.detail.tabelog_rating != -1 &&
+                <div className={styles.menuItem}>
+                  <img className={styles.logoImg} src='./assets/img/logos/tabelogLogo.png'/> {this.props.detail.tabelog_rating}
+                  <br/>
                 </div>
-             </div>
-           <div id="Reviews">
-              {detail.reviews &&
-                detail.reviews.map((review,i) => {
-                return(
-                    <Review key={i} review={review}/>
-                );
-                })
               }
-           </div>
+              <div className={styles.menuItem}>
+                <img className={styles.logoImg} src='./assets/img/logos/tripadvisorLogo.png'/> {this.props.detail.tripadvisor_rating}
+              </div>
+            </div>
+          </div>
+
+          <div id="Reviews">
+            {detail.reviews &&
+              detail.reviews.map((review,i) => {
+                return(
+                  <Review key={i} review={review}/>
+                );
+            })}
+          </div>
         </div>
       );
   }
